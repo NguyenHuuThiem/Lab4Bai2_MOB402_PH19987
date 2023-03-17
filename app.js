cba0462fc5ea5236f7c8c6163489a08da11a2177
thiem <nguyenhuuthiem03112003@gmail.com>
2023-03-17 08:30:42 +0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+
+
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 var app = express();
-app.listen(process.env.PORT || '4000'); 
+app.listen(process.env.PORT || '4000');
 
 
-app.engine('.hbs', exphbs());
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'page2',
+    layoutsDir: __dirname + '/views/layouts',
+
+}))
 app.set('view engine', '.hbs');
 
 
-app.get('/hbs', function(req, res){
-    res.render('index');
+app.get('/tinhToan', function (req, res) {
+    res.render('defaultView', {
+        layout: 'main',
+        soA: 10,
+        soB: 7,
+        kq: 17,
+        operator: 'cong',
+        showBody: false,
+        showContentTinhToan: true,
+        // shoTitle: false,
+    });
+});
+
+app.get('/', function (req, res) {
+    res.render('index', { 
+        layout: 'main',
+        showBody: true,
+        showContentTinhToan: false, 
+    });
 });

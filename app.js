5d68c9ceb6c63befafa78a507782a3c012e477d4
thiem <nguyenhuuthiem03112003@gmail.com>
2023-03-19 20:50:36 +0700

    update

--- app.js
+++ app.js
@@ -9,30 +9,36 @@ app.listen(process.env.PORT || '4000');
 
 app.engine('.hbs', exphbs({
     extname: '.hbs',
-    defaultLayout: 'page2',
+    defaultLayout: 'mayTinh',
     layoutsDir: __dirname + '/views/layouts',
 
 }))
 app.set('view engine', '.hbs');
 
 
-app.get('/tinhToan', function (req, res) {
-    res.render('defaultView', {
-        layout: 'main',
-        soA: 10,
-        soB: 7,
-        kq: 17,
-        operator: 'cong',
-        showBody: false,
-        showContentTinhToan: true,
-        // shoTitle: false,
-    });
-});
+// app.get('/tinhToan', function (req, res) {
+//     res.render('defaultView', {
+//         layout: 'main',
+//         soA: 10,
+//         soB: 7,
+//         kq: 17,
+//         operator: 'cong',
+//         showBody: false,
+//         showContentTinhToan: true,
+//         // shoTitle: false,
+//     });
+// });
 
 app.get('/', function (req, res) {
     res.render('index', { 
-        layout: 'main',
-        showBody: true,
-        showContentTinhToan: false, 
+        layout: 'mayTinh',
+        // showBody: true,
+        // showContentTinhToan: false, 
     });
 });
+
+// app.get('/mayTinh', function (req, res) {
+//     res.render('mayTinh', { 
+//         layout: 'main',
+//     });
+// });
